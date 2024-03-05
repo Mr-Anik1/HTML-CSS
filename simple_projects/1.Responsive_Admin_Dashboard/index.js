07bf3f38709eb6e26567c6907dd1005bcf1f677f
Anik <anik2002bro@gmail.com>
2024-03-06 04:45:30 +0600

    Pirates Admin Dashboard project has been completed successfully

---
+++ simple_projects/1.Responsive_Admin_Dashboard/index.js
@@ -0,0 +1,42 @@
+const sideMenu = document.querySelector("aside");
+const menuBTN = document.querySelector("#menu-btn");
+const closeBTN = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+
+// Open Sidebar
+menuBTN.addEventListener("click", () => {
+  sideMenu.style.display = "block";
+});
+
+// Close Sidebar
+closeBTN.addEventListener("click", () => {
+  sideMenu.style.display = "none";
+});
+
+// Change Theme
+themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme-variables");
+
+  themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+  themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+});
+
+// Fill Product Orders
+orders.forEach((order) => {
+  const tr = document.createElement(`tr`);
+  const trContent = `
+                <td>${order.productName}</td>
+                <td>${order.productNumber}</td>
+                <td>${order.paymentStatus}</td>
+                <td class="${
+                  order.shipping === "Declined"
+                    ? `danger`
+                    : order.shipping === "Pending"
+                    ? `warning`
+                    : `primary`
+                }">${order.shipping}</td>
+                <td class="primary">Details</td>
+    `;
+  tr.innerHTML = trContent;
+  document.querySelector("table tbody").appendChild(tr);
+});
---
+++ simple_projects/1.Responsive_Admin_Dashboard/orders.js
@@ -0,0 +1,38 @@
+const orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: 85631,
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Lavander KF-102 Drone",
+    productNumber: 36095,
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "Ruko F11 Pro Drone",
+    productNumber: 20590,
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Camera Drone",
+    productNumber: 46680,
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "GPS 4k Drone",
+    productNumber: 24839,
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Pine Phone Pro",
+    productNumber: 72984,
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+];
